c1e4da5ef3116b6ee9838ea6ffad5860ef916e89
Hugo Lamarre <c-hlamarre@touchtunes.com>
2022-12-14 13:57:34 -0500

    Initial Commit

---
+++ Pipeline/PipelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  PipelineApp.swift
+//  Pipeline
+//
+//  Created by TouchTunes on 2022-12-14.
+//
+
+import SwiftUI
+
+@main
+struct PipelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

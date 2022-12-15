264efacc3238b2c51340a92a95126d75d9fe8bc0
Hugo Lamarre <c-hlamarre@touchtunes.com>
2022-12-15 11:37:09 -0500

    Removed google service

--- Pipeline/PipelineApp.swift
+++ Pipeline/PipelineApp.swift
@@ -11,14 +11,14 @@ import FirebaseCore
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
+    //FirebaseApp.configure()
     return true
   }
 }
 
 @main
 struct PipelineApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    //@UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
